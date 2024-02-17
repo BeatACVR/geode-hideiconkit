fc16d17e7e3780fe4c3a6e71805f2b615a4d88c0
Beat <75857504+BeatACVR@users.noreply.github.com>
2024-02-16 20:39:44 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) 
+      return false;
+		
+		auto button = this->getChildByID("icon-kit-button");
+		auto modEnabled = Mod::get()->getSettingValue<bool>("mod-enabled");
+		
+		if(modEnabled){
+			button->setVisible(false);
+		}
+
+		return true;
+	} 
+};
